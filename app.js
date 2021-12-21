65f07d3c273f6eeec010e8b35201b94d2a8401cf
AllTeach <33581448+AllTeach@users.noreply.github.com>
2021-12-21 10:02:45 +0200

    Added a comment
    no rooms

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ wss.on('connection', function connection(ws) {
     ws.on('message', function incoming(message) {
         console.log('received: %s', message);
         // Broadcast to everyone else.
+		// for the moment no rooms
         wss.clients.forEach(function each(client) {
             if (/*client !== ws &&*/ client.readyState === WebSocket.OPEN) {
                 client.send(message);
